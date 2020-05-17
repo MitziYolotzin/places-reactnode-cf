4a1778a011ea8c849bbc3042d3b175b989a34069
MitziYolotzin <mitziulter@gmail.com>
2020-05-17 14:22:33 -0500

    crud, routes

--- api/app.js
+++ api/app.js
@@ -5,20 +5,16 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 
-const Place = require('./models/Place');
+//models
+
+//routes
+const places = require('./routes/places');
 
 const db = require('./config/database');
-
 db.connect();
-
-//var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -27,87 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-//app.use('/', indexRouter);
-//app.use('/users', usersRouter);
 //
-app.post('/places', (req, res) => {
-    Place.create({
-            title: req.body.title,
-            description: req.body.description,
-            acceptsCreditCard: req.body.acceptsCreditCard,
-            openHour: req.body.openHour,
-            closeHour: req.body.closeHour
-        })
-        .then(doc => {
-            res.json(doc)
-        }).catch(err => {
-            console.log(err);
-            res.json(err);
-        });
-});
-
-app.get('/places', (req, res) => {
-    Place.find({
-
-        })
-        .then(docs => {
-            res.json(docs);
-        })
-        .catch(err => {
-            console.log(err);
-            res.json(err);
-        })
-});
-
-app.get('/places/:id', (req, res) => {
-    //res.json(req.params.id)
-    //Place.findOne({})
-    Place.findById(req.params.id)
-        .then(doc => {
-            res.json(doc);
-        })
-        .catch(err => {
-            console.log(err);
-            res.json(err);
-        })
-});
-
-app.put('/places/:id', (req, res) => {
-    // Place.findById(req.params.id)
-    //     .then(doc => {
-    //         doc.title = req.body.title;
-    //         doc.description = req.body.description;
-    //         doc.save();
-    //     })
-    let attributes = ['title', 'description', 'acceptsCreditCard', 'openHour', 'closeHour'];
-    let placeParams = {};
-    attributes.forEach(attr => {
-        if (Object.prototype.hasOwnProperty.call(req.body, attr))
-            placeParams[attr] = req.body[attr];
-    });
-    //methods: update, findOneAndUpdate , findByIdAndUpdate
-    Place.findOneAndUpdate({ '_id': req.params.id }, placeParams, { new: true })
-        .then(doc => {
-            res.json(doc);
-        })
-        .catch(err => {
-            console.log(err);
-            res.json(err);
-        })
-
-})
-
-app.delete('/places/:id', (req, res) => {
-    Place.findByIdAndRemove(req.params.id)
-        .then(doc => {
-            res.json({})
-        })
-        .catch(err => {
-            console.log(err);
-            res.json(err);
-        })
-})
+app.use('/places', places);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
