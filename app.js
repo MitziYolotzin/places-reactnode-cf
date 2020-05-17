c1ece6ff58bccc694a76a3bad445391649f3dec6
MitziYolotzin <mitziulter@gmail.com>
2020-05-16 19:42:48 -0500

    server test express

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const app = express();
+
+const places = [{
+        'title': 'Oficina',
+        'description': 'Lorem',
+        'address': 'Ipsum'
+    },
+    {
+        'title': 'Oficina 2',
+        'description': 'Lorem',
+        'address': 'Ipsum'
+    }
+]
+
+app.get('/', (req, res) => {
+    res.json({
+        places
+    });
+});
+
+
+app.post('/', (req, res) => {
+    res.json({
+        'metodo': 'POST'
+    })
+});
+
+app.use(express.static('public'));
+
+app.listen(3000, function() {
+    console.log('Server on')
+})
